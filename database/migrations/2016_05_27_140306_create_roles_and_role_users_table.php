a01621f18456fee7cf4385313fc95dd03cf212f4
kia <cendiqkrn@gmail.com>
2016-05-27 21:06:34 +0700

    add roles and role users table

---
+++ database/migrations/2016_05_27_140306_create_roles_and_role_users_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRolesAndRoleUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('slug')->unique();
+            $table->string('name');
+            $table->text('permissions')->nullable();
+            $table->timestamps();
+
+            $table->engine = 'InnoDB';
+        });
+
+        Schema::create('role_users', function (Blueprint $table) {
+            $table->integer('user_id')->unsigned();
+            $table->integer('role_id')->unsigned();
+            $table->nullableTimestamps();
+
+            $table->engine = 'InnoDB';
+            $table->primary(['user_id', 'role_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('role_users');
+        Schema::drop('roles');
+    }
+}
