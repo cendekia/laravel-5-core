b7b83f5cd541d5fa1aa57816624f50df447e8e17
kia <cendiqkrn@gmail.com>
2016-06-03 18:53:05 +0700

    add 403, 404, 505 admin error page

---
+++ resources/views/errors/403_admin.blade.php
@@ -0,0 +1,13 @@
+@extends('admin.layouts.master')
+
+@section('content')
+    <div class="deep-purple-700 bg-big">
+        <div class="text-center">
+            <h1 class="text-shadow no-margin text-white text-4x p-v-lg">
+                <span class="text-2x font-bold m-t-lg block">403</span>
+            </h1>
+            <h2 class="h1 m-v-lg">RESTRICTED!</h2>
+            <p class="h4 m-v-lg text-u-c font-bold">You're not authorized to accessing this page.</p>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/errors/505_admin.blade.php
@@ -0,0 +1,13 @@
+@extends('admin.layouts.master')
+
+@section('content')
+    <div class="deep-purple-700 bg-big">
+        <div class="text-center">
+            <h1 class="text-shadow no-margin text-white text-4x p-v-lg">
+                <span class="text-2x font-bold m-t-lg block">404</span>
+            </h1>
+            <h2 class="h1 m-v-lg">OUCH!</h2>
+            <p class="h4 m-v-lg text-u-c font-bold">DON'T WORRY, WE WILL FIX IT SOON.</p>
+        </div>
+    </div>
+@endsection
