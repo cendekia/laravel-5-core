cb2f8d8294a916d926183924afe8f008847d5cd6
kia <cendiqkrn@gmail.com>
2016-06-07 11:22:19 +0700

    add redirection and error message at verify token

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
 
 class VerifyCsrfToken extends BaseVerifier
@@ -14,4 +15,14 @@ class VerifyCsrfToken extends BaseVerifier
     protected $except = [
         //
     ];
+
+    public function handle($request, Closure $next){
+        if($request->input('_token')) {
+            if ( \Session::getToken() != $request->input('_token')) {
+                return redirect()->back()->withInput(\Request::except('_token'))->withErrors('Your session has expired. Please try again, your session has been restored now.');
+            }
+        }
+
+        return parent::handle($request, $next);
+    }
 }
