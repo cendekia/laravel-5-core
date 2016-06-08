de0e81bb99253d5fb3b0b2bc4d720bfc9f91996d
kia <cendiqkrn@gmail.com>
2016-06-08 12:25:46 +0700

    fix misplacing model

--- app/Http/Controllers/Admin/SubscribeController.php
+++ app/Http/Controllers/Admin/SubscribeController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Admin\Controller;
 use App\Http\Requests;
-use App\Model\Subscribe;
+use App\Models\Subscribe;
 use Illuminate\Http\Request;
 
 class SubscribeController extends Controller
